7f359a698c6b486d9d31a52476a0bdcfbd96d32c
= <Israel.arauj0@live.com>
2021-03-27 19:53:40 -0300

    Added UsersController

---
+++ PerformanceMonitor.API/Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace PerformanceMonitor.API.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    public class UsersController : Controller
+    {
+        [HttpGet("")]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok();
+        }
+    }
+}
