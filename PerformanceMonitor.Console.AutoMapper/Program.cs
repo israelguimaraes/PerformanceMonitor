cafd053ddeddf62b045dbbe12d94c77065256ffe
= <Israel.arauj0@live.com>
2021-04-22 22:30:02 -0300

    added console projects

---
+++ PerformanceMonitor.Console.AutoMapper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PerformanceMonitor.Console.AutoMapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ PerformanceMonitor.Console.Mapster/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PerformanceMonitor.Console.Mapster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
